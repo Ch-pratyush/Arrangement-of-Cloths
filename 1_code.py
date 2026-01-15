4b15b62be436d0a1bc80affc85991b2fa869016e
Ch-pratyush <pratyush.25178@ee.du.ac.in>
2026-01-16 04:31:44 +0530

    Add files via upload

---
+++ 1_code.py
@@ -0,0 +1,10 @@
+shelves = 4
+almirah = {
+    1: "Seasonal Wear",
+    2: "Daily Wear",
+    3: "Formal Wear",
+    4: "Traditional Wear"
+}
+print("Almirah Arrangement:")
+for shelf in almirah:
+    print("Shelf", shelf, ":", almirah[shelf])
---
+++ 2_code.py
@@ -0,0 +1,11 @@
+luggage = {
+    "Bottom": "Shoes",
+    "Lower Middle": "Daily Clothes",
+    "Upper Middle": "Formal Clothes",
+    "Top": "Toiletries and Accessories"
+}
+
+# Display luggage arrangement
+print("Luggage Packing Arrangement:")
+for position in luggage:
+    print(position, ":", luggage[position])
